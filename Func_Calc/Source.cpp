a88fab5f6e493bb57af54b1ef5f4b3a373b3cc22
2GAPUH <svetoslavvolkov2004@gmail.com>
2023-03-24 22:34:49 +0300

    НЕбольшие изменения + возможность вводить и отрисовывать стандартные функции

--- Func_Calc/Source.cpp
+++ Func_Calc/Source.cpp
@@ -12,7 +12,6 @@
 #define SEGMENT 10
 #define STEP 0.01
 
-
 SDL_Window* win = NULL;
 SDL_Renderer* ren = NULL;
 
@@ -49,63 +48,6 @@ void Init()
 		printf_s("Couldn't create renderer! Error: %s", SDL_GetError());
 		system("pause");
 		DeInit(1);
-	}
-
-}
-
-bool IsNumber(char symb)
-{
-	if (symb == '0' || symb == '1' || symb == '2' || symb == '3' || symb == '4' || symb == '5' || symb == '6' || symb == '7' || symb == '8' || symb == '9')
-		return 1;
-	return 0;
-}
-
-bool IsArithmetic(char symb)
-{
-	if (symb == '+' || symb == '-' || symb == '*' || symb == '/')
-		return 1;
-	return 0;
-}
-
-bool IsVariable(char symb)
-{
-	if (symb == 'x' || symb == 'y')
-		return 1;
-	return 0;
-}
-
-void AddEnd(char func[])
-{
-	for (int i = 0;i < ARRAY_SIZE;i++)
-	{
-		if (func[i] == 0)
-			func[i] = '\n';
-	}
-}
-
-void GetNumbers(char func[], int numbers[])
-{
-	for (int i = 0, temp = 0, n = 0; func[i] != '\n';i++)
-	{
-		if (IsNumber(func[i]))
-			temp = temp * 10 + func[i] - '0';
-		
-		else
-		{
-			numbers[n++] = temp;
-			temp = 0;
-		}
-
-		if(func[i+1] == '\n')
-		{
-			numbers[n++] = temp;
-			temp = 0;
-		}
-
-
-
-		if (func[i + 1] == '\n')
-			numbers[n] = INT_MAX;
 	}
 
 }
@@ -153,30 +95,216 @@ void DrawAxis()
 	SDL_RenderDrawLine(ren, WINDOW_WIDTH, WINDOW_HEIGHT / 2, WINDOW_WIDTH - WINDOW_WIDTH / 50, WINDOW_HEIGHT / 2 - WINDOW_HEIGHT / 30);
 }
 
-void DrawGraph()
+void ValueSubstitution(double tempNum[], double x)
 {
+	for (int i = 0;tempNum[i] != INT_MAX;i++)
+	{
+		if (tempNum[i] == INT_MIN)
+			tempNum[i] = x;
+	}
+}
+
+
+
+
+int IsNumber(char symb)
+{
+	if (symb == '0' || symb == '1' || symb == '2' || symb == '3' || symb == '4' || symb == '5' || symb == '6' || symb == '7' || symb == '8' || symb == '9')
+		return 1;
+	if (symb == 'x' || symb == 'X')
+		return 2;
+	return 0;
+}
+
+bool IsArithmetic(char symb)
+{
+	if (symb == '+' || symb == '-' || symb == '/' || symb == '*' || symb == '^' )
+		return 1;
+	return 0;
+}
+
+void GetFunc(char func[])
+{
+	int i = -1;
+	char temp = 0;
+	for (i; temp != 13; )
+	{
+		temp = _getch();
+		if (IsArithmetic(temp) || IsNumber(temp))
+		{
+			i++;
+			printf_s("%c", temp);
+			func[i] = temp;
+			continue;
+			
+		}
+		
+	}
+	func[i+1] = '\n';
+	printf_s("\n");
+}
+
+void GetNumbers(char func[], double numbers[])
+{
+	int n = 0;
+	int temp;
+	for (int i = 0;func[i] != '\n';i++)
+	{
+		temp = IsNumber(func[i]);
+		if (temp == 1)
+		{
+			numbers[n] = numbers[n]*10 + func[i] - '0';
+		}
+		else if(temp == 2)
+		{
+			numbers[n] = INT_MIN;
+		}
+		else  n++;
+
+	}
+	numbers[n+1] = INT_MAX;
+}
+
+void GetArithmetic(char func[], char arithmetic[])
+{
+	int n = 0;
+	for (int i = 0;func[i] != '\n';i++)
+	{
+		if (IsArithmetic(func[i]))
+		{
+			arithmetic[n++] = func[i];
+		}
+	}
+	arithmetic[n] = '\n';
+}
+
+
+void CopyArrayNum(double numbers[], double tempNum[])
+{
+	int i = 0;
+	for (i; numbers[i] != INT_MAX;i++)
+		tempNum[i] = numbers[i];
+	tempNum[i] = INT_MAX;
+}
+
+
+
+void Calculations(char arithmetic[], double tempNum[])
+{
+	double temp = 0, temp1 = 0;
+
+	for (int i = 0; arithmetic[i] != '\n';i++)
+	{
+		if (arithmetic[i] == '^')
+		{
+			int n = i;
+			while (tempNum[n] == INT_MAX - 1)
+				n--;
+			temp = tempNum[i];
+			for (int n = 1;n < tempNum[i + 1];n++)
+				tempNum[i] *= temp;
+			tempNum[i + 1] = INT_MAX - 1;
+		}
+	}
+
+	for (int i = 0; arithmetic[i] != '\n';i++)
+	{
+		if (arithmetic[i] == '*')
+		{
+			int n = i;
+			while (tempNum[n] == INT_MAX - 1)
+				n--;
+			tempNum[n] *= tempNum[i + 1];
+			tempNum[i + 1] = INT_MAX - 1;
+		}
+		if (arithmetic[i] == '/')
+		{
+			int n = i;
+			while (tempNum[n] == INT_MAX - 1)
+				n--;
+			if (tempNum[i + 1] == 0)
+			{
+				tempNum[n] = 3;
+				tempNum[i + 1] = INT_MAX - 1;
+				continue;
+			}
+			tempNum[n] /= tempNum[i + 1];
+			tempNum[i + 1] = INT_MAX - 1;
+		}
+	}
+
+
+	for (int i = 0; arithmetic[i] != '\n';i++)
+	{
+		if (arithmetic[i] == '+')
+		{
+			int n = i;
+			while (tempNum[n] == INT_MAX-1)
+				n--;
+			tempNum[n] += tempNum[i + 1];
+			tempNum[i + 1] = INT_MAX - 1;
+		}
+		if (arithmetic[i] == '-')
+		{
+			int n = i;
+			while (tempNum[n] == INT_MAX - 1)
+				n--;
+			tempNum[n] -= tempNum[i + 1];
+			tempNum[i + 1] = INT_MAX - 1;
+		}
+	}
+
+}
+
+void DrawGraph(char arithmetic[], double numbers[], double tempNum[], double start = (-WINDOW_WIDTH / 2 / SEGMENT), double stop = WINDOW_WIDTH / 2 / SEGMENT)
+{
+	if (start < (-WINDOW_WIDTH / 2 / SEGMENT))
+		start = (-WINDOW_WIDTH / 2 / SEGMENT);
+
+	if (stop > WINDOW_WIDTH / 2 / SEGMENT)
+		stop = WINDOW_WIDTH / 2 / SEGMENT;
+
 	SDL_SetRenderDrawColor(ren, 128, 0, 128, 0);
 
-	double tempX1 = -WINDOW_WIDTH / 2 / SEGMENT, tempX = 0, tempY = 0;
-	double tempY1 = sin(tempX1);
+	double tempX1 = start, tempX = 0, tempY = 0;
+	double tempY1 = -1;
 
-	for (double i = -WINDOW_WIDTH/2/SEGMENT; i <= WINDOW_WIDTH / 2 / SEGMENT; i += STEP)
+	for (double i = start; i <= stop; i += STEP)
 	{
-		//SDL_RenderDrawPoint(ren, i * SEGMENT + WINDOW_WIDTH / 2, -cos(i) * SEGMENT + WINDOW_HEIGHT / 2);
-
+		if (i == 0)
+			continue;
+		CopyArrayNum(numbers, tempNum);
+		ValueSubstitution(tempNum, i);
+		Calculations(arithmetic, tempNum);
 
 		tempX = tempX1;
 		tempY = tempY1;
 		tempX1 = i;
-		tempY1 = -sin(tempX1);
+		tempY1 = -tempNum[0];
+		if (tempY1 > WINDOW_HEIGHT / SEGMENT)
+			tempY1 = WINDOW_HEIGHT / SEGMENT;
 		SDL_RenderDrawLine(ren, tempX * SEGMENT + WINDOW_WIDTH / 2, tempY * SEGMENT + WINDOW_HEIGHT / 2, tempX1 * SEGMENT + WINDOW_WIDTH / 2, tempY1 * SEGMENT + WINDOW_HEIGHT / 2);
 	}
 }
+
 
 int main(int argc, char* argv[])
 {
 	system("chcp 866");
 	system("cls");
+	char func[ARRAY_SIZE] = {};
+	char arithmetic[ARRAY_SIZE / 2] = {};
+	double numbers[ARRAY_SIZE] = {};
+	double tempNum[ARRAY_SIZE] = {};
+	
+	GetFunc(func);
+
+	GetNumbers(func, numbers);
+
+	GetArithmetic(func, arithmetic);
+
+	
+
 	Init();
 
 	SDL_SetRenderDrawColor(ren, 255, 255, 255, 0);
@@ -184,7 +312,7 @@ int main(int argc, char* argv[])
 
 	DrawAxis();
 
-	DrawGraph();
+	DrawGraph(arithmetic , numbers, tempNum);
 	
 	SDL_RenderPresent(ren);
 
@@ -193,27 +321,3 @@ int main(int argc, char* argv[])
 	DeInit(0);
 	return 0;
 }
-
-/*char func[ARRAY_SIZE] = {};
-	int numbers[ARRAY_SIZE];
-	int otvet = 0;
-
-	printf_s("Enter func: ");
-	while (getchar() != '\n');
-	gets_s(func);
-
-	AddEnd(func);
-
-	GetNumbers(func, numbers);
-
-
-	for (int i = 0, temp = numbers[0], n = 1;func[i] != '\n';i++)
-	{
-		
-		if (IsArithmetic(func[i]) && func[i] != '(')
-		{
-			numbers[n++];
-			if (func[i] == '+')
-
-		}
-	}*/
